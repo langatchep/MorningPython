68ee30fdd72398aaf733bd021b68d9121f021b0e
langatchep <joycheptoo418@gmail.com>
2026-02-03 10:26:32 +0300

    initial commit

---
+++ introduction.py
@@ -0,0 +1,6 @@
+#print is a method used to display an output
+
+print("Welcome to Python Programming")
+print("Joyline")
+print(100)
+print(20*3)
---
+++ variables.py
@@ -0,0 +1,16 @@
+#variable is a container used to store a value
+
+school = "eMobilis"
+firstname = "Joyline"
+lastname ="Cheptoo"
+num1 = 36
+num2 = 42
+language = "Python"
+
+print(school)
+print(firstname,lastname)
+print(num1 + num2)
+print(language,"is a web programming language")
+
+#The sum of 36 and 42 is 78
+print("The sum of",num1,"and",num2,"is",num1+num2)
