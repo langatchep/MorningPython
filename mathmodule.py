9fb956d68448ef398b60738b045854fcf45b7ac5
langatchep <joycheptoo418@gmail.com>
2026-02-09 09:38:50 +0300

    initial commit

---
+++ mathmodule.py
@@ -0,0 +1,6 @@
+import math
+x = math.sqrt(169)
+print("The square root of 169 is", x)
+
+y = math.ceil(56.78)
+print("The ceil is", y)
--- module.py
+++ module.py
@@ -1,3 +1,7 @@
-import functions
+# Module is any python file that has a function.
+
+import functions,employeesdetails
+
 
 functions.dog("Max","Siberian Husky","3")
+employeesdetails.display_employee_details("Victor Kiiru","CEO","Male","25")
